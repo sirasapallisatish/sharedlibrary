c2ad3a977d51f1817fdcc6f93ec208c1d0dbc424
sirasapallisatish <152602173+sirasapallisatish@users.noreply.github.com>
2023-12-21 16:10:15 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,9 @@ def contDeploy(jobname,ip,appname)
 {
       sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+
+def conttesting(repo)
+{
+      git "https://github.com/intelliqittrainings/${repo}"
+}
+
